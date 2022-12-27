7300c4461d9ed40b5b2b1e9ba0d491e10c2972db
lilApril <aprilnya@yandex.ru>
2022-12-28 00:07:55 +0300

    mainwindow useable

---
+++ anothergamefiles/main.py
@@ -0,0 +1,35 @@
+from machine import SlotMachine
+from settings import *
+import pygame
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Слоты')
+        self.bg_image = pygame.image.load(bg_image).convert_alpha()
+        self.m = SlotMachine()
+        self.clock = pygame.time.Clock()
+        self.delta_time = 0
+
+    def run(self):
+
+        self.start_time = pygame.time.get_ticks()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+
+            self.delta_time = (pygame.time.get_ticks() - self.start_time) / 1000
+            self.start_time = pygame.time.get_ticks()
+            pygame.display.update()
+            self.screen.blit(self.bg_image, (0, 0))
+            self.m.update(self.delta_time)
+            self.clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
--- mainwindow.py
+++ mainwindow.py
@@ -1,15 +1,17 @@
 import pygame
 import casinosocket
-
+from main import Game
 
 class Board:
-    def __init__(self, width, height):
+    def __init__(self, width, height, screen):
         self.width = width
         self.height = height
         self.board = [[0] * width for _ in range(height)]
         self.left = 10
         self.top = 10
         self.cell_size = 30
+        self.screen = screen
+        self.hide = False
 
     def set_view(self, left, top, cell_size):
         self.left = left
@@ -20,7 +22,21 @@ class Board:
         wcolor = pygame.Color("white")
         for i in range(self.height):
             for j in range(self.width):
-                pygame.draw.rect(surface, wcolor,
+                if i == 0 and j == 0:
+                    gameicon = pygame.image.load('slotsicon.jpg').convert()
+                    gameicon.set_colorkey((255, 255, 255))
+                    obj_rect = gameicon.get_rect(
+                        center=(self.left + self.cell_size * j + self.cell_size // 2, self.top + self.cell_size * i + self.cell_size // 2))
+                    pygame.display.update()
+                    scale = pygame.transform.scale(
+                        gameicon, (self.cell_size, self.cell_size))
+
+                    scale_rect = scale.get_rect(
+                        center=(self.left + self.cell_size * j + self.cell_size // 2, self.top + self.cell_size * i + self.cell_size // 2))
+
+                    self.screen.blit(scale, scale_rect)
+                else:
+                    pygame.draw.rect(surface, wcolor,
                                  (self.left + self.cell_size * j, self.top + self.cell_size * i,
                                   self.cell_size, self.cell_size), 1 if self.board[i][j] == 0 else 0)
 
@@ -42,12 +58,18 @@ class Board:
                                                                 1] < self.top + self.height * self.cell_size else None
 
     def on_click(self, cords):
-        print((cords[0] // self.cell_size - self.top // self.cell_size,
-               cords[1] // self.cell_size - self.top // self.cell_size) if 0 <= cords[
-            0] // self.cell_size - self.top // self.cell_size < self.width and 0 <= cords[
+        coords = (cords[0] // self.cell_size - self.top // self.cell_size,
+        cords[1] // self.cell_size - self.top // self.cell_size if 0 <= cords[
+        0] // self.cell_size - self.top // self.cell_size < self.width and 0 <= cords[
                                                                                1] // self.cell_size - self.top // \
                                                                            self.cell_size < \
-                                                                           self.height else None)
+                                                                     self.height else None)
+        if coords == (0, 0):
+            slotwindow = Game()
+            slotwindow.run()
+            self.hide = True
+
+
 
 class mainwindow:
     def __init__(self, id, username, password, chips):
@@ -56,14 +78,15 @@ class mainwindow:
         self.password = password
         self.chips = chips
         pygame.init()
-        self.screen = pygame.display.set_mode((384, 135))
 
     def render(self):
         pygame.init()
         size = 1800, 800
         screen = pygame.display.set_mode(size)
         pygame.display.set_caption("Queen of spades")
-        board = Board(8, 3)
+        logo = pygame.image.load('logopic.jpg').convert()
+        pygame.display.set_icon(logo)
+        board = Board(8, 3, screen)
         board.set_view(0, 0, 100)
         running = True
         while running:
@@ -72,7 +95,11 @@ class mainwindow:
                     running = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     board.on_click(event.pos)
-                #board.render(screen)
+                    if board.hide:
+                        pygame.display.quit()
+                        running = False
+                        break
+
                 bgimage = pygame.image.load('mainbackground.png').convert()
                 sprite = bgimage
                 sprite.set_colorkey((255, 255, 255))
@@ -87,7 +114,13 @@ class mainwindow:
                 screen.blit(scale, scale_rect)
 
                 pygame.display.update(obj_rect)
+                board.render(screen)
+
 
 
                 pygame.display.flip()
-        pygame.quit()
+        if running == False and board.hide == True:
+            board.hide = False
+            self.render()
+        else:
+            pygame.quit()
