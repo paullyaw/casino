ae6bbeff1b3d66c8a972daab2556fdcdb913631a
paullyaw <116737190+paullyaw@users.noreply.github.com>
2022-12-29 01:42:07 +0300

    Add files via upload

---
+++ slots3/settings.py
@@ -0,0 +1,18 @@
+# Настройки экрана
+DEFAULT_IMAGE_SIZE = (300, 300)
+FPS = 120
+HEIGHT = 1000
+WIDTH = 1600
+START_X, START_Y = 0, -300
+X_OFFSET, Y_OFFSET = 20, 0
+
+# Картинки
+bg_image = 'pic/pictures/bg.png'
+GAME_INDICES = [1, 2, 3]
+
+symbols = {
+    'diamond': f"pic/pictures/symbols/5.png",
+    'floppy': f"pic/pictures/symbols/1.png",
+    'hourglass': f"pic/pictures/symbols/3.png",
+    'hourglass2': f"pic/pictures/symbols/2.png",
+    'telephone': f"pic/pictures/symbols/4.png"}
