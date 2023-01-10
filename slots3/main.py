579a969014cf9f186ab3a7d04365ba4ce7219784
paullyaw <116737190+paullyaw@users.noreply.github.com>
2023-01-10 16:55:40 +0300

    Add files via upload

--- slots3/main.py
+++ slots3/main.py
@@ -6,28 +6,32 @@ import pygame
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption('Слоты')
-        self.bg_image = pygame.image.load(bg_image).convert_alpha()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))  # размер окна
+        pygame.display.set_caption("Слоты")  # название
+        bg_image = "pic/pictures/bg.jpg"
+        self.bg_image = pygame.image.load(bg_image).convert_alpha()  # установка заднего фона
         self.m = SlotMachine()
         self.clock = pygame.time.Clock()
         self.delta_time = 0
+        self.time = pygame.time.get_ticks()
+        self.sound()  # начальная мелодия при открытии игры
 
     def run(self):
-
-        self.start_time = pygame.time.get_ticks()
-
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
-
-            self.delta_time = (pygame.time.get_ticks() - self.start_time) / 1000
-            self.start_time = pygame.time.get_ticks()
-            pygame.display.update()
+            self.delta_time = (pygame.time.get_ticks() - self.time) / 1000
+            self.time = pygame.time.get_ticks()
+            pygame.display.flip()
             self.screen.blit(self.bg_image, (0, 0))
             self.m.update(self.delta_time)
             self.clock.tick(FPS)
+
+    def sound(self):
+        # музыка
+        start_sound = pygame.mixer.Sound('sounds/start_sound.mp3')
+        start_sound.play()
 
 
 if __name__ == '__main__':
--- slots3/reel.py
+++ slots3/reel.py
@@ -26,7 +26,7 @@ class Reel:
                 reel_is_stopping = True
             if self.delay_time <= 0:
                 for symbol in self.symbol_list:
-                    symbol.rect.bottom += 100
+                    symbol.rect.bottom += 50
                     if symbol.rect.top == 1200:
                         if reel_is_stopping:
                             self.reel_is_spinning = False
@@ -35,19 +35,47 @@ class Reel:
                         self.symbol_list.add(
                             Symbol(symbols[random.choice(self.shuffled_keys)], ((symbol.x_val), -300), symbol_idx))
 
+    # звук прокрутки
+    def sound(self):
+        reel_sound = pygame.mixer.Sound('sounds/reel.wav')
+        reel_sound.set_volume(0.2)
+        reel_sound.play()
+
     def scroll_on(self, delay_time):
+        self.sound()
         self.delay_time = delay_time
         self.spin_time = 1000 + delay_time
         self.reel_is_spinning = True
 
+    # результат вращения по индексам символов
     def spin_result(self):
-        pass
+        spin_symbols = []
+        for i in GAME_INDICES:
+            spin_symbols.append(self.symbol_list.sprites()[i].sym_type)
+        return spin_symbols[::-1]
 
 
 class Symbol(pygame.sprite.Sprite):
     def __init__(self, pathToFile, pos, idx):
         super().__init__()
+        self.sym_type = pathToFile.split('/')[3].split('.')[0]
         self.pos, self.idx = pos, idx
         self.image = pygame.image.load(pathToFile).convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
         self.x_val = self.rect.left
+
+
+def flip_horizontal(result):
+    horizontal_values = []
+    l3 = []
+    for value in result.values():
+        horizontal_values.append(value)
+    row, col = len(horizontal_values), len(horizontal_values[0])
+    l2 = [[""] * row for k in range(col)]
+    for x in range(row):
+        for y in range(col):
+            l2[y][row - x - 1] = horizontal_values[x][y]
+    for i in l2:
+        l3.append(i[::-1])
+    print(l3)
+    return l3
